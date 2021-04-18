6603f4c808c1100537fdb132e85ecf173e43298c
Balázs Szeidl <szeidl.balazs@hotmail.com>
2021-04-18 18:17:14 +0200

    StorePicker component ready and works fine.

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import StorePicker from './Components/StorePicker';
+
+function App() {
+  return (
+    <div className="App">
+      <StorePicker/>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/Components/StorePicker.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class StorePicker extends React.Component {
+
+	render() {
+		return (
+			<p>
+				I am the StorePicker!
+			</p>
+
+		);
+	}
+}
+
+export default StorePicker;
