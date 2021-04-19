bf40b32d54d75dd5a2b37bc210bfe96a5949ff2c
Balázs Szeidl <szeidl.balazs@hotmail.com>
2021-04-19 15:22:51 +0200

    Header component destrucutred to stateless functional component.

--- frontend/src/Components/Header.jsx
+++ frontend/src/Components/Header.jsx
@@ -1,28 +1,26 @@
 import React from 'react';
 
-class Header extends React.Component {
+const Header = (props) => {
+	return (
+		<header className="top">
+			
+			<h1>
+				Catch
+				<span className="ofThe">
+					<span className="of">Of</span>
+					<span className="the">The</span>
+				</span>
+				Day
+			</h1>
+			
+			<h3 className="tagline">
+				<span>{props.tagline}</span>
+			</h3>
 
-	render () {
+		</header>
+	);
 
-		return (
-			<header className="top">
-				
-				<h1>
-					Catch
-					<span className="ofThe">
-						<span className="of">Of</span>
-						<span className="the">The</span>
-					</span>
-					Day
-				</h1>
-				
-				<h3 className="tagline">
-					<span>{this.props.tagline}</span>
-				</h3>
-
-			</header>
-		)
-	}
 }
+
 
 export default Header;
