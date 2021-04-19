33c1028b91c57d2a76b1112f8ae5f501c0892290
Balázs Szeidl <szeidl.balazs@hotmail.com>
2021-04-19 23:20:03 +0200

    Router component with switches ready

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+/*import App from './App';*/
 import reportWebVitals from './reportWebVitals';
+import Router from './Components/Router';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router />
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -15,3 +16,11 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+/*
+  <React.StrictMode>
+    <Router />
+  </React.StrictMode>,
+
+*/
