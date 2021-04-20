eb8a8a8203b162f17da607f4f6f0011da49ccf2a
Balázs Szeidl <szeidl.balazs@hotmail.com>
2021-04-20 10:00:10 +0200

    Event handler arrow function to StorePicker has been created.

--- frontend/src/Components/StorePicker.jsx
+++ frontend/src/Components/StorePicker.jsx
@@ -3,11 +3,23 @@ import { getFunName } from './Helpers';
 
 class StorePicker extends React.Component {
 
+	myInput=React.createRef();
+
+	goToStore = (event) => {
+		event.preventDefault();
+		console.log(this.myInput);
+	}
+
 	render() {
 		return (
-			<form className="store-selector">
+			<form className="store-selector" onSubmit={this.goToStore}>
 				<h2>Please Enter A Store</h2>
-				<input type="text" required placeholder="Store Name" defaultValue={getFunName()}/>
+				<input 
+					type="text" 
+					required placeholder="Store Name" 
+					defaultValue={getFunName()}
+					ref={this.myInput}
+				/>
 				<button type="submit">{`Visit Store \u279c`}</button>
 			</form>		
 		);
